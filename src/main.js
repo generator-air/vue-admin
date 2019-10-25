f4a3467ab496f26b0d6da70ed29bf9a0fd4620dc
fightingliu(刘兴) <fightingliu@tencent.com>
2019-10-25 16:32:54 +0800

    feat emonitor

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import $ElementUI from 'element-ui';
 import $App from '@/App.vue';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import * as $emonitor from '@tencent/emonitor'
 
 $vue.use($ElementUI);
+$vue.use($emonitor);
 
 new $vue({
   el: '#app',
