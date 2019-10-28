fc07fb1bea1f83aa2bfdcbb2de83ea3987a47ae2
arczhang <arczhang@tencent.com>
2019-10-28 19:14:45 +0800

    chore
    add loaders, support pug and less

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+#app
+	img(alt="Vue logo" src="./assets/logo.png")
+	HelloWorld(msg="Welcome to Your Vue.js App")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
   }
-};
+}
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
