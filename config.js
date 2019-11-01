280c99ce18c5f43b2d0752b619887461561f0e83
arczhang <arczhang@tencent.com>
2019-11-01 16:13:13 +0800

    chore: 合并 dev

---
+++ config.js
@@ -0,0 +1,47 @@
+const $urlJoin = require('url-join');
+const $path = require('path');
+
+const config = {};
+
+// 任务运行时环境，保存任务执行期间一些环境参数
+config.context = {};
+config.root = $path.resolve(__dirname);
+
+// 源代码路径
+config.src = 'src';
+// 初步构建生成路径
+config.dist = 'dist';
+// 版本库忽略目录的路径，也作为构建流程的临时目录
+config.ignore = 'ignore';
+// 版本库外部引入文件路径
+config.externals = 'externals';
+// cdn部署路径
+config.cdnBase = '//cdn.jia.qq.com';
+// 创建项目后，需要更改这个属性，规则需要遵循前端资源映射规范
+config.uploadUrl = '/2019/vue-admin';
+config.cdnRoot = $urlJoin(config.cdnBase, config.uploadUrl);
+
+// 开发服务端口
+config.devServerPort = 8090;
+// 本地mock服务端口
+config.mockServerPort = 8091;
+
+
+config.mock = "https://tmock.qq.com/mock/271";
+
+config.production = "https://pes.dayuw.cn";
+
+config.development = "http://localhost:" + config.devServerPort;
+
+
+// cdn上传配置
+config.uploadConfig = {
+	AppId: '1256737511',
+	SecretId: 'AKID02ub8UafZJsHZQDqLfomfT6YpyxlRwIU',
+	SecretKey: 'AAyc8TcaQEwUs570iFbmneJn6EF5lGb4',
+	Bucket: 'qlink',
+	Region: 'ap-chengdu',
+	prefix: config.uploadUrl
+};
+
+module.exports = config;
--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
+import $vue from 'vue';
+import $elementUI from 'element-ui';
+import $app from '@/App.vue';
+import $emontior from '@/util/emontior';
+// import $store from './store' // 暂未使用，先注释
+import $router from './router' // 暂未使用，先注释
+
+import 'element-ui/lib/theme-chalk/index.css';
 import './mixin'
 import './assets/icon'
 import '../node_modules/normalize.css/normalize.css'
 
-Vue.config.productionTip = false
+$vue.prototype.project = 'vue-admin'; // 项目名称
 
-new Vue({
-  store,
-  router,
-  render: h => h(App)
-}).$mount('#app')
+$vue.use($elementUI);
+// 使用emontior
+$vue.use($emontior);
+
+new $vue({
+	router: $router,
+  el: '#app',
+  render: h => h($app)
+});
