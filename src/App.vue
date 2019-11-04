3af03a1835bdce19ef394cd396306452c2d70c66
fightingliu(刘兴) <fightingliu@tencent.com>
2019-11-04 12:24:11 +0800

    fix:修复app.vue报错

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,16 @@
 			.body-content
 				v-demo
 				.l-bread
-					el-breadcrumb(
-						separator="/"
+				el-breadcrumb(
+					separator="/"
+					v-if="$route.meta.breadcrumb"
 					)
-						el-breadcrumb-item(
-							:key="bread"
-							:to="{ name: bread }"
-						) {{bread}}
-					//- h3 {{$route.name}}
+					el-breadcrumb-item(
+						v-for="bread in $route.meta.breadcrumb"
+						:key="bread"
+						:to="{ name: bread }"
+					) {{bread}}
+				h3 {{$route.name}}
 				router-view.l-route
 			v-copyright
 </template>
