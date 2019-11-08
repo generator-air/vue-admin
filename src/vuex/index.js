3b541fb5ac165a1bfab9be490ab5d4cda83b2c5d
arczhang <arczhang@tencent.com>
2019-11-08 13:58:53 +0800

    fix: 修改vuex模块，给了个简单的例子

--- src/vuex/index.js
+++ src/vuex/index.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import user from './user'
 
 Vue.use(Vuex)
 
+const modules = {
+	user
+}
+
+// 遍历modules对象，为每个子对象添加namespaced属性
+Object.keys(modules).forEach(obj => {
+	modules[obj].namespaced = true
+})
+
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-		user: {
-			namespaced: true
-		}
-  }
+  modules
 })
