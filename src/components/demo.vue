9e42746ab7b8e909caf456e19d1932be60f6ae96
fightingliu <codingliu@qq.com>
2019-11-14 17:38:14 +0800

    fix：添加跨域请求测试示例

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script>
-
+import $axios from 'axios'
 import $env from '@/model/env';
 import $config from '../../config';
 export default {
@@ -18,12 +18,23 @@ export default {
 	computed: {},
 	data() { return {} },
 	methods: {
+		milldeWare() {
+       // 以下跨域请求测试
+        $axios.get(this.HOST + '/v2/movie/imdb/tt0111161', {
+            params: {
+                "apikey": "0df993c66c0c636e29ecbb5344252a4a"
+            }
+        }).then((response) => {
+            console.log("get:" + response.data);
+        });
+		},
 		ageisReport() {
 			this.$aegis.logE('aegis异常日志上报');
 			this.$aegis.logI('aegis普通日志上报')
 		},
 		init() {
 			console.log('当前请求域名', $env.domain);
+			this.milldeWare();
 			if ($config.logReport) {
 				this.ageisReport();
 			}
