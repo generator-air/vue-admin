7c5aa96f17680e43610264dce15af5012faaa31c
arczhang <arczhang@tencent.com>
2019-11-14 14:40:24 +0800

    fix: 重写request模块，精简了不必要的错误处理和参数检查，错误码字典改由配置文件引入

--- src/mixin/request.js
+++ src/mixin/request.js
@@ -1,159 +1,63 @@
 import axios from 'axios'
 import type from '@/util/type'
-import get from 'lodash/get'
+import errorDict from '../model/errorDict'
 
-let errorDict = {}
-/**
- * {
- * 	403: 'Error happened',
- *  404: () => jump(),
- *  203: {
- * 		3000: 'error code is 3000',
- * 		4000: () => console.log('error code is 4000')
- *  }
- * }
- */
-
-const NO_RESPONSE = '没有收到响应数据，请检查请求是否跨域和链接是否正确'
-const NET_ERROR_MSG = '请求失败，请检查网络 '
-const DATA_TYPE_ERROR = '返回数据格式错误'
-const URL_FORMAT_ERROR = '请求地址格式错误，请检查'
-const REQUEST_PARAMETER_ERROR = '请求参数错误，请检查'
-const ERROR_CODE_DICT_TYPE_ERROR = '错误码字典的类型必须是Object'
-
-const reject = reason => Promise.reject(reason || NET_ERROR_MSG)
-
-const getMethod = opt => {
-	let method = ''
-	if (opt.method && type(opt.method) === 'string') {
-		// 传过来的配置里有 method 项并且为字符串，则为用户手动指定 method
-		method = opt.method.toLowerCase()
+const doWith = sth => {
+	const sType = type(sth)
+	if (sType === 'string') {
+		return sth
+	} else if (sType == 'function') {
+		return sth()
 	} else {
-		method = 'get'
-	}
-	return method
-}
-
-// 检查传入的 option 的 url 是否合法
-const legalUrl = url => {
-	let legal = false
-	if (type(url) !== 'string') {
-		legal = false
-	} else {
-		legal = /^https?:\/\//.test(url)
-	}
-	if (legal) {
-		return url
-	} else {
-		throw new Error(URL_FORMAT_ERROR)
-	}
-}
-
-const doWith = (sth, spare) => {
-	// 处理多类型数据
-	sth = sth || spare
-	const sthType = type(sth)
-	switch (sthType) {
-		case 'string':
-		case 'number':
-			return sth
-		case 'function':
-			return sth()
-		default:
-			return 'TYPE ERROR'
+		return false
 	}
 }
 
 const useDict = (status, code, spare) => {
-	let dictMatch = null
-	if (code) {
-		dictMatch = get(errorDict, [status, code])
-	} else {
-		dictMatch = get(errorDict, [status])
+	let dictMatch = errorDict[status]
+	const statusType = type(dictMatch)
+	if (statusType === 'object') {
+		dictMatch = errorDict[status][code]
 	}
-	return doWith(dictMatch, spare) || NET_ERROR_MSG + status
+	return Promise.reject(doWith(dictMatch) || spare)
 }
 
-// 需要保证请求的地址为绝对地址（http:// or https://）
-function req (options) {
-	let req = null
-	const optType = type(options)
-	if (optType === 'string') {
-		// 直接过来一个 url 地址
-		req = axios(legalUrl(options))
-	} else if (optType === 'object') {
-		// 参数是个 Obj
-		const requestOption = {
-			method: getMethod(options),
-			data: options.data || {},
-			url: options.url,
-			// 处理跨域
-			withCredentials: false,
-			// 是否自定义错误处理流程
-			customError: options.customError || false
-		}
-		legalUrl(options.url)
-		if (requestOption.method === 'get') {
-			requestOption.params = options.data || {}
-		} else if (requestOption.method === 'post') {
-			requestOption.params = options.data || {}
-		}
-		req = axios(requestOption)
-	} else {
-		// 参数错误(这里是直接return一个reject还是throw一个error比较好？)
-		throw new Error(REQUEST_PARAMETER_ERROR)
+// 全局拦截器
+axios.interceptors.response.use(({ data, status }) => {
+	if (data.code === 0 && status === 200) {
+		// 没有错误的理想情况直接返回 payload
+		return data.data
 	}
-	return req.then(res => {
-		const { status, data: rowData } = res
-		// 这里有点不明白 option 配置中的 customError 有什么用，栋哥写的还是 axios 的配置项？
-		if (options.row || options.custom || options.customError) {
-			return rowData
-		}
-		if (!rowData) {
-			return reject(NET_ERROR_MSG + status)
-		}
-		if (status === 200 && Number(rowData.code) === 0) {
-			return rowData.data
-		}
-		if (type(rowData !== 'object')) {
-			return reject(DATA_TYPE_ERROR)
-		}
-		return reject(useDict(status, rowData.code, '请求失败：' + rowData.msg))
-	}).catch(err => {
-		if (err.response) {
-			// status 为 200 以外的并且带有相应的情况
-			// 如果跨域则不会有响应
-			// 存在响应的时候，响应可以是 JSON，也可以是标签字符串等
-			const status = err.response.status
-			const code = get(err.response, 'data.code')
-			return reject(useDict(status, code, NET_ERROR_MSG + status))
-		}
-		return Promise.reject(NO_RESPONSE)
-	})
-}
+	// 不是理想情况的，需要使用错误码字典
+	return useDict(status, data.code, data.msg || data)
+}, err => {
+	if (err.response) {
+		const { status, data } = err.response
+		return useDict(status, data.code, data.msg || err.message)
+	}
+	return Promise.reject(err.message)
+})
 
 const request = Vue => {
-	Vue.prototype.$request = req
-	Vue.prototype.$setErrorDict = function (dict) {
-		if (type(dict) !== 'object') {
-			throw new Error(ERROR_CODE_DICT_TYPE_ERROR)
+	Vue.prototype.get = (url, params) => axios({
+		url,
+		params,
+		method: 'get',
+		headers: {
+			// ajax 请求标识，部分服务器会区别对待 ajax 请求和普通请求
+			'X-Requested-With': 'XMLHttpRequest'
 		}
-		errorDict = dict
-	}
-	Vue.prototype.$get = function (url, params = {}) {
-		return this.$request({
-			url,
-			params,
-			method: 'get'
-		})
-	}
-	Vue.prototype.$post = function (url, data = {}) {
-		return this.$request({
-			url,
-			data,
-			method: 'post'
-		})
-	}
+	})
+	Vue.prototype.post = (url, data) => axios({
+		url,
+		data,
+		method: 'post',
+		headers: {
+			'X-Requested-With': 'XMLHttpRequest',
+			// 标准 HTML form 格式
+			'Content-Type': 'application/x-www-form-urlencoded'
+		}
+	})
 }
 
 export default request
