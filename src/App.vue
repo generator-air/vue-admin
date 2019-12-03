2d56646ed0cf6bb761e54602560d82bf71a98efc
fightingliu <codingliu@qq.com>
2019-12-03 11:12:36 +0800

    feat:日期处理工具封装以及demo开发

--- src/App.vue
+++ src/App.vue
@@ -31,6 +31,7 @@ import navSide from '@/components/navSide'
 import copyright from '@/components/copyright'
 import logo from '@/components/logo'
 import $config from '../config'
+import $date from '@/util/date'
 
 export default {
 	components: {
@@ -54,7 +55,27 @@ export default {
 	methods: {
 		toggleLeftStatus (status) {
 			this.leftStatus = status
-		}
+		},
+		dateFormat(time, type) {
+		    let date = {}
+		    switch (type) {
+						case 'sec':
+						    date = 	$date.formatSec(time);
+						    break;
+            case 'day':
+                date = 	$date.formatDay(time);
+                break;
+            case 'secText':
+                date = 	$date.formatSecText(time);
+                break;
+            case 'quantum':
+                date = 	$date.formatQuantum(time);
+                break;
+						default:
+						    break;
+        }
+        return date
+		},
 	},
 	created () {
 		// https://yapi.qqmylife.com/mock/227/rule/rules/clearing/final
@@ -72,6 +93,11 @@ export default {
 			// 监控当前页面
 			this.$aegis.logI('aegis普通日志上报')
 		}
+		const testTime = '1575341866'
+		console.log('时间格式化\n秒：' + this.dateFormat(testTime, 'sec')
+				+ '\n天：' + this.dateFormat(testTime, 'day')
+				+ '\n标准时间：' + this.dateFormat(testTime, 'secText')
+				+ '\n年月日：' + this.dateFormat(testTime, 'quantum'))
 	}
 }
 </script>
