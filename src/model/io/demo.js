3bebe59508282f1470e339f19ed7a4e43e96fafd
fightingliu <codingliu@qq.com>
2019-12-04 12:21:08 +0800

    refactor::移除冗余代码

--- src/model/io/demo.js
+++ src/model/io/demo.js
@@ -1,12 +1,11 @@
 import $request from '@/mixin/request'
 import $api from './api'
-import $notify from '@/util/notify'
 
 const Api = {}
 
 
 // 根据字段查询,key为空全局搜索
 Api.list = para => $request.$get($api['list'], para).catch(
-	err => $notify.error(err)
+	err => console.error(err)
 )
 export default Api
--- src/pages/menu3/list.vue
+++ src/pages/menu3/list.vue
@@ -117,222 +117,227 @@
 </template>
 
 <script>
-    import { mapState } from 'vuex';
-    import $search from '@/components/search'
-    import $filter from '@/components/filter';
-    import $pagination from '@/components/pagination'
-    import $table from '@/components/table'
-    import $apiQuest from '@/model/io/demo'
-    import $selectOptions from '@/util/selectOptions'
-    export default {
-        components: {
-            'v-search': $search,
-            'v-filter': $filter,
-            'v-table': $table,
-            'v-pagination': $pagination,
-        },
-        watch: {
-            $route() {
-                this.init();
-                // 如果当前用户有权访问所有人创建的文章
-                if (this.userOperation.all && !this.$route.query.list) {
-                    this.$router.push({ query: { list: 'all' } });
-                    // 重置过滤项，避免显示上一数据页下拉选项
-                    this.$refs.filter.reset();
-                }
-            }
-        },
-        computed: {
-            ...mapState('user', {
-                auth: state => state.auth
-            })
-        },
-        data() {
-            return {
-                type: '',
-                id: 1,
-                selectCount: 0,
-                selectRead: 0,
-                articleType: '',
-                api: $apiQuest.list,
-                stateOptions: $selectOptions.COMMON_STATE,
-                operations: [],
-                tableSelections: [],
-                searchValue: '',
-                total: 0,
-                page: 0,
-                limit: 5,
-                dialogTable: {
-                    list: [],
-                    total: 0,
-                    page: 1
-                },
-                para: {
-                    is_approved: true,
-                    reason: ''
-                },
-                userOperation: {},
-            };
-        },
-        methods: {
-            // 查询
-            search() {
-                this.$refs.search.search();
-            },
-            // 重置
-            reset() {
-                this.$refs.search.clear();
-                this.$router.push({ path: this.$route.path });
-            },
-            // 编辑
-            edit(row) {
-                alert('编号:' + row.id + ' , 编辑操作')
-            },
-            // 删除
-            del(row) {
-                alert('编号:' + row.id + ' , 删除操作')
-            },
-            operationHandler(row, operation) {
-                let confirmText = '';
-                let messageText = '';
-                if (operation === 'submit') {
-                    confirmText = '确认要提交审核？';
-                    messageText = '文章已提交审核';
-                } else {
-                    confirmText = '确认要撤回数据？';
-                    messageText = '数据已撤回';
-                }
-                this.$confirm(confirmText, '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(async () => {
-                    let rs = {};
-                    if (operation === 'submit') {
-                        await $apiQuest.submit(row.qid, this.para);
-                    } else {
-                        await $apiQuest.revert(row.qid);
-                    }
-                    if (rs) {
-                        this.$message({
-                            type: 'success',
-                            message: messageText
-                        });
-                        // 刷新列表
-                        this.$refs.list.update();
-                    }
-                }).catch(() => {});
-            },
-            batchHandler(command) {
-                let operationName = '';
-                let confirmText = '';
-                let messageText = '';
-                const ids = [];
-                // 操作名称确认
-                if (command.name.includes('submit')) {
-                    operationName = 'submit';
-                    confirmText = '确认要提交审核？';
-                    messageText = '文章已提交审核';
-                } else {
-                    operationName = 'withdraw';
-                    confirmText = '确认要撤回数据？';
-                    messageText = '数据已撤回';
-                }
-                // 操作文章id整合
-                this.tableSelections.forEach(item => {
-                    ids.push(item.qid);
-                });
-                this.$confirm(confirmText, '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(async () => {
-                    const para = {
-                        operation: operationName,
-                        reason: '',
-                        id_list: ids
-                    };
-                    let rs = {};
-                    if (operationName === 'submit') {
-                        rs = await $apiQuest.batchSubmit(para);
-                    } else {
-                        rs = await $apiQuest.batchWithdraw(para);
-                    }
-                    if (rs) {
-                        this.$message({
-                            type: 'success',
-                            message: messageText
-                        });
-                        // 刷新列表
-                        this.$refs.list.update();
-                    }
-                }).catch(() => {});
-            },
-            // 列表选中项变更
-            selectionChangeHandler(val) {
-                let countCommandOne = 0;
-                let countCommandTwo = 0;
-                this.selectCount = val.length;
-                // 没有选中时清空一下数据
-                let selectRead = 0;
-                console.log(val)
-                val.forEach(item => {
-                    // if (item.state === 0 || item.state === 3 || item.state === 4) {
-                    // 		// 第一类操作命令
-                    // 		++countCommandOne;
-                    // } else {
-                    // 		// 第二类操作命令
-                    // 		++countCommandTwo;
-                    // }
-                    selectRead += item.id;
-                });
-                if (val.length > 0) {
-                    if (countCommandOne === val.length) {
-                        this.operations = [{ id: 'submit', method: this.submit, label: '提交审核' }];
-                    } else if (countCommandTwo === val.length) {
-                        this.operations = [{ id: 'withdraw', method: this.withdraw, label: '撤回数据' }];
-                    } else {
-                        this.operations = [];
-                    }
-                } else {
-                    this.operations = [];
-                }
-                this.tableSelections = val;
-                this.selectRead = selectRead / 10000;
-            },
-            // 列表数据变更
-            onListChange(rs) {
-                if (rs) {
-                    this.total = rs.total;
-                }
-            },
-            notify($title, $message, $type) {
-                this.$notify({
-                    title: $title,
-                    message: $message,
-                    type: $type
-                });
-            },
-            init() {
-                // 搜索项回显
-                this.searchValue = this.$route.query.search;
-            }
-        },
-        // 当前list页面挂载前赋值this.api，保证table挂载时拿到指定的api
-        created() {
-            this.init();
-        },
-        mounted() {
-            // 过滤项回显。mounted前无法通过 this.$refs 访问组件
-            this.$refs.filter.update();
-            // 如果当前用户有权访问所有人创建的文章
-            if (this.userOperation.all && !this.$route.query.list) {
-                this.$router.push({ query: { list: 'all' } });
-                // 重置过滤项，避免显示上一文章页下拉选项
-                this.$refs.filter.reset();
-            }
-        }
-    };
+import { mapState } from 'vuex';
+import $search from '@/components/search'
+import $filter from '@/components/filter';
+import $pagination from '@/components/pagination'
+import $table from '@/components/table'
+import $apiQuest from '@/model/io/demo'
+export default {
+		components: {
+				'v-search': $search,
+				'v-filter': $filter,
+				'v-table': $table,
+				'v-pagination': $pagination,
+		},
+		watch: {
+				$route() {
+						this.init();
+						// 如果当前用户有权访问所有人创建的文章
+						if (this.userOperation.all && !this.$route.query.list) {
+								this.$router.push({ query: { list: 'all' } });
+								// 重置过滤项，避免显示上一数据页下拉选项
+								this.$refs.filter.reset();
+						}
+				}
+		},
+		computed: {
+				...mapState('user', {
+						auth: state => state.auth
+				})
+		},
+		data() {
+				return {
+						type: '',
+						id: 1,
+						selectCount: 0,
+						selectRead: 0,
+						articleType: '',
+						api: $apiQuest.list,
+						stateOptions: {
+								0: '草稿',
+								1: '待审核',
+								2: '已发布',
+								3: '审核拒绝',
+								4: '已撤回'
+						},
+						operations: [],
+						tableSelections: [],
+						searchValue: '',
+						total: 0,
+						page: 0,
+						limit: 5,
+						dialogTable: {
+								list: [],
+								total: 0,
+								page: 1
+						},
+						para: {
+								is_approved: true,
+								reason: ''
+						},
+						userOperation: {},
+				};
+		},
+		methods: {
+				// 查询
+				search() {
+						this.$refs.search.search();
+				},
+				// 重置
+				reset() {
+						this.$refs.search.clear();
+						this.$router.push({ path: this.$route.path });
+				},
+				// 编辑
+				edit(row) {
+						alert('编号:' + row.id + ' , 编辑操作')
+				},
+				// 删除
+				del(row) {
+						alert('编号:' + row.id + ' , 删除操作')
+				},
+				operationHandler(row, operation) {
+						let confirmText = '';
+						let messageText = '';
+						if (operation === 'submit') {
+								confirmText = '确认要提交审核？';
+								messageText = '文章已提交审核';
+						} else {
+								confirmText = '确认要撤回数据？';
+								messageText = '数据已撤回';
+						}
+						this.$confirm(confirmText, '提示', {
+								confirmButtonText: '确定',
+								cancelButtonText: '取消',
+								type: 'warning'
+						}).then(async () => {
+								let rs = {};
+								if (operation === 'submit') {
+										await $apiQuest.submit(row.qid, this.para);
+								} else {
+										await $apiQuest.revert(row.qid);
+								}
+								if (rs) {
+										this.$message({
+												type: 'success',
+												message: messageText
+										});
+										// 刷新列表
+										this.$refs.list.update();
+								}
+						}).catch(() => {});
+				},
+				batchHandler(command) {
+						let operationName = '';
+						let confirmText = '';
+						let messageText = '';
+						const ids = [];
+						// 操作名称确认
+						if (command.name.includes('submit')) {
+								operationName = 'submit';
+								confirmText = '确认要提交审核？';
+								messageText = '文章已提交审核';
+						} else {
+								operationName = 'withdraw';
+								confirmText = '确认要撤回数据？';
+								messageText = '数据已撤回';
+						}
+						// 操作文章id整合
+						this.tableSelections.forEach(item => {
+								ids.push(item.qid);
+						});
+						this.$confirm(confirmText, '提示', {
+								confirmButtonText: '确定',
+								cancelButtonText: '取消',
+								type: 'warning'
+						}).then(async () => {
+								const para = {
+										operation: operationName,
+										reason: '',
+										id_list: ids
+								};
+								let rs = {};
+								if (operationName === 'submit') {
+										rs = await $apiQuest.batchSubmit(para);
+								} else {
+										rs = await $apiQuest.batchWithdraw(para);
+								}
+								if (rs) {
+										this.$message({
+												type: 'success',
+												message: messageText
+										});
+										// 刷新列表
+										this.$refs.list.update();
+								}
+						}).catch(() => {});
+				},
+				// 列表选中项变更
+				selectionChangeHandler(val) {
+						let countCommandOne = 0;
+						let countCommandTwo = 0;
+						this.selectCount = val.length;
+						// 没有选中时清空一下数据
+						let selectRead = 0;
+						console.log(val)
+						val.forEach(item => {
+								// if (item.state === 0 || item.state === 3 || item.state === 4) {
+								// 		// 第一类操作命令
+								// 		++countCommandOne;
+								// } else {
+								// 		// 第二类操作命令
+								// 		++countCommandTwo;
+								// }
+								selectRead += item.id;
+						});
+						if (val.length > 0) {
+								if (countCommandOne === val.length) {
+										this.operations = [{ id: 'submit', method: this.submit, label: '提交审核' }];
+								} else if (countCommandTwo === val.length) {
+										this.operations = [{ id: 'withdraw', method: this.withdraw, label: '撤回数据' }];
+								} else {
+										this.operations = [];
+								}
+						} else {
+								this.operations = [];
+						}
+						this.tableSelections = val;
+						this.selectRead = selectRead / 10000;
+				},
+				// 列表数据变更
+				onListChange(rs) {
+						if (rs) {
+								this.total = rs.total;
+						}
+				},
+				notify($title, $message, $type) {
+						this.$notify({
+								title: $title,
+								message: $message,
+								type: $type
+						});
+				},
+				init() {
+						// 搜索项回显
+						this.searchValue = this.$route.query.search;
+				}
+		},
+		// 当前list页面挂载前赋值this.api，保证table挂载时拿到指定的api
+		created() {
+				this.init();
+		},
+		mounted() {
+				// 过滤项回显。mounted前无法通过 this.$refs 访问组件
+				this.$refs.filter.update();
+				// 如果当前用户有权访问所有人创建的文章
+				if (this.userOperation.all && !this.$route.query.list) {
+						this.$router.push({ query: { list: 'all' } });
+						// 重置过滤项，避免显示上一文章页下拉选项
+						this.$refs.filter.reset();
+				}
+		}
+};
 </script>
 
 <style lang="less" scoped>
