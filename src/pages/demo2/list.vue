5d40aa7761fa503008bdbf2de29ab080f200d1ef
fightingliu <codingliu@qq.com>
2019-12-05 18:23:44 +0800

    fix:添加组件调用示例

--- src/pages/demo2/list.vue
+++ src/pages/demo2/list.vue
@@ -1,17 +1,44 @@
 <template lang="pug">
 	#app1
-		span 组件调用示例
+		.u-style.u-table-header
+			el-card(class="box-card")
+				h3.u-style.header-title 组件调用示例
+					div(class="text item")
+						h5.u-style.header-title {{"测试时间: " +this.time}}
+						h5.u-style.header-title {{"时间格式化(秒): "  +this.sec}}
+						h5.u-style.header-title {{"天: " +this.day}}
+						h5.u-style.header-title {{"标准时间: " +this.sect}}
+						h5.u-style.header-title {{"年月日: " + this.quant}}
+			.u-style.u-table-header
+			el-card(class="box-card")
+				h3.u-style.header-title 网络请求示例
+					div(class="text item")
+						h5.u-style.header-title {{"接口地址: " +this.url}}
+						h5.u-style.header-title 返回数据：
+						h5.u-style.header-title {{this.list}}
 </template>
 
 <script>
 import { mapMutations, mapState } from 'vuex'
 import $config from '../../../config'
 import $date from '@/util/date'
+
 export default {
 		computed: {
 				...mapState('user', [
 						'userInfo'
 				])
+		},
+		data() {
+				return {
+						time: '',
+						sec: '',
+						day: '',
+						sect: '',
+						quant: '',
+						list: {},
+						url: '',
+				}
 		},
 		methods: {
 				...mapMutations('user', [
@@ -26,16 +53,16 @@ export default {
 						let date = {}
 						switch (type) {
 								case 'sec':
-										date = 	$date.formatSec(time);
+										date = $date.formatSec(time);
 										break;
 								case 'day':
-										date = 	$date.formatDay(time);
+										date = $date.formatDay(time);
 										break;
 								case 'secText':
-										date = 	$date.formatSecText(time);
+										date = $date.formatSecText(time);
 										break;
 								case 'quantum':
-										date = 	$date.formatQuantum(time);
+										date = $date.formatQuantum(time);
 										break;
 								default:
 										break;
@@ -48,8 +75,10 @@ export default {
 				// 404 response
 				// https://www.gamersky.com/news/201911/123w7764.shtml
 				/* req demo */
-				this.$get('https://yapi.qqmylife.com/mock/227/rule/rules/clearing/final')
+				this.url = 'https://yapi.qqmylife.com/mock/227/rule/rules/clearing/final';
+				this.$get(this.url,)
 						.then((e) => {
+						    this.list = e;
 								console.log('%c' + JSON.stringify(e, null, 2), 'color:violet')
 						})
 				/* aegis log demo */
@@ -59,11 +88,11 @@ export default {
 						// 监控当前页面
 						this.$aegis.logI('aegis普通日志上报')
 				}
-				const testTime = '1575341866'
-				console.log('时间格式化\n秒：' + this.dateFormat(testTime, 'sec')
-						+ '\n天：' + this.dateFormat(testTime, 'day')
-						+ '\n标准时间：' + this.dateFormat(testTime, 'secText')
-						+ '\n年月日：' + this.dateFormat(testTime, 'quantum'))
+				this.time = '1575341866'
+				this.sec = this.dateFormat(this.time, 'sec');
+				this.day = this.dateFormat(this.time, 'day')
+				this.sect = this.dateFormat(this.time, 'secText')
+				this.quant = this.dateFormat(this.time, 'quantum')
 		}
 }
 </script>
