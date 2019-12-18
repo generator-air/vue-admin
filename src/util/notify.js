6a02c6939b7e1cbbe1f7e4257bfe633603c70850
shryzhang <996707719@qq.com>
2019-12-18 17:10:47 +0800

    perf: 删除冗余代码

--- src/util/notify.js
+++ src/util/notify.js
@@ -1,14 +1,9 @@
 import $bus from '@/mixin/bus'
-import $logger from '@/util/logger'
-
-const logger = $logger('util/notify')
 
 const notify = (msg, options) => {
 	if (msg && msg.toString) {
 		msg = msg.toString()
 	}
-
-	logger.info('send:', msg, options)
 
 	let para = Object.assign({
 		title: '消息',
